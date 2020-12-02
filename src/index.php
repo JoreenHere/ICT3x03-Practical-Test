75eef87d8067c36820dd05a87cc3fe9fe08a0777
JoreenHere <54747149+JoreenHere@users.noreply.github.com>
2020-12-02 12:41:57 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Home Page</title>
     </head>
     <body>
         <?php
