91a1a5a1ccbeacb6d3718b9b66e53ed1b1854415
JoreenHere <54747149+JoreenHere@users.noreply.github.com>
2020-12-02 13:12:16 +0800

    Update welcome.php

--- src/welcome.php
+++ src/welcome.php
@@ -4,7 +4,16 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<?php session_start() ?>
+<?php 
+
+session_start(); 
+
+    if(isset($_POST["logout"])){
+        session_destroy();
+        header("location: index.php");
+    }
+
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,8 +21,10 @@ and open the template in the editor.
     </head>
     <body>
         <div>
-            <p><?php echo $_SESSION["pwd"] ?></p>
-
+            <p><?php echo $_SESSION["pwd"] ?></p><br>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <input type="submit" name="logout" value="Logout">
+            </form>
         </div>
     </body>
 </html>
